0bb50a900fafe6ac8a9a379518cf63827a93cfe0
Keerthy Baby <keerthybaby025@gmail.com>
2024-11-10 14:26:18 +0530

    give active class to a navbar link

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,6 +1,7 @@
-import Link from 'next/link';
+
 import React from 'react';
 import styles from "./links.module.css"
+import NavLink from './navLink/navLink';
 
 const Links = () => {
 
@@ -22,11 +23,24 @@ const Links = () => {
       path:"/blog"
     },
   ];
+
+  // TEMPORARY
+  const session=true;
+  const isAdmin=true;
+
   return (
     <div className={styles.links}>
       {links.map((link=>(
-       <Link href={link.path} key={link.title}>{link.title}</Link> 
+       <NavLink item={link} key={link.title}/>
       )))}
+      {session ? (
+        <>
+        {isAdmin && <NavLink item={{title:"Admin",path:"/admin"}} />}
+        <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{title:"Login",path:"/login"}}/>
+      )}
     </div>
   );
 };
