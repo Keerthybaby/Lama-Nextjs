67b51f00f2122c395d624b773392f5d668547196
Keerthy Baby <keerthybaby025@gmail.com>
2024-11-12 22:11:01 +0530

    Image

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import Image from "next/image";
+import React from "react";
+import styles from "./about.module.css"
 const AboutPage = () => {
   return (
     <div>
-      AboutPage
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/29130848/pexels-photo-29130848/free-photo-of-serene-pine-forest-with-ocean-view.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt=""  fill />
+      </div>
     </div>
   );
 };
