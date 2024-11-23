fb8a9d1b4e315140a55cbe789f473f6d25f4ff4f
Keerthy Baby <keerthybaby025@gmail.com>
2024-11-23 17:21:39 +0530

    Add about page design

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,29 @@
+import Image from "next/image";
+import styles from "./home.module.css";
 const Home = () => {
-  
-  return <div>Homepage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Creqtive Thoughts Agency.</h1>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam est,
+          optio dolorem labore saepe laborum nesciunt ratione recusandae
+          repellendus odio, pariatur nulla quaerat sunt enim fuga quae
+          similique. Suscipit, molestiae.
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg}/>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
