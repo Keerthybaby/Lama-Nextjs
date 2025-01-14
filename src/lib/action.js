ce66649c0b051f7b4219a44a5dee3e0bd9d70cf7
Keerthy Baby <keerthybaby025@gmail.com>
2025-01-14 10:32:57 +0530

    useForm Hook

--- src/lib/action.js
+++ src/lib/action.js
@@ -58,12 +58,12 @@ export const handleLogout = async () => {
   console.log("User logged out");
 };
 
-export const register = async (formData) => {
+export const register = async (previousState, formData) => {
   const { username, email, password, img, passwordRepeat } =
     Object.fromEntries(formData);
 
   if (password !== passwordRepeat) {
-    return "Passwords do not match";
+    return { error: "Passwords do not match" };
   }
 
   try {
@@ -71,7 +71,7 @@ export const register = async (formData) => {
     const user = await User.findOne({ username });
 
     if (user) {
-      return "Username already exists";
+      return { error: "Username already exists" };
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -85,19 +85,24 @@ export const register = async (formData) => {
 
     await newUser.save();
     console.log("saved to db");
+    return { success: true };
   } catch (err) {
     console.log(err);
     return { error: "Something went wrong" };
   }
 };
 
-export const login = async (formData) => {
+export const login = async (prevState, formData) => {
   const { username, password } = Object.fromEntries(formData);
 
   try {
     await signIn("credentials", { username, password });
   } catch (err) {
     console.log(err);
+
+    if (err.type === "CredentialsSignin")  {
+      return { error: "Invalid username or password" };
+    }
     return { error: "Something went wrong" };
   }
 };
