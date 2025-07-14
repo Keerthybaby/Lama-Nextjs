f2ad1318125f6131c43729456a8d2b1953b2f39d
Keerthy Baby <keerthybaby025@gmail.com>
2025-07-14 11:24:12 +0530

    code updated

--- next.config.js
+++ next.config.js
@@ -1,14 +1,48 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // images: {
+  //   // remotePatterns: [
+  //   //   // {
+  //   //   //   protocol: "https",
+  //   //   //   hostname: "images.pexels.com",
+  //   //   // },
+  //   //   // {
+  //   //   //   protocol: "https",
+  //   //   //   hostname: "png.pngtree.com",
+  //   //   // },
+
+  //   //   // {
+  //   //   //   protocol: "https",
+  //   //   //   hostname: "**.natgeofe.com", // Allow any subdomain
+  //   //   // },
+  //   //   {
+  //   //     protocol: "https",
+  //   //     hostname: "**", // Allow all remote images
+  //   //   },
+
+  //   // ],
+  //   // domains: [
+  //   //   "images.pexels.com",
+  //   //   "i.natgeofe.com",
+  //   //   "png.pngtree.com",
+  //   //   "www.tourism-of-india.com",
+  //   //   "i.etsystatic.com",
+  //   //   "dynamic-media-cdn.tripadvisor.com",
+  //   // ],
+    
+  // },
+  // images: {
+  //   unoptimized: true, // ✅ Disables image optimization globally
+  // },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.pexels.com",
+        protocol: 'https',
+        hostname: '**', // Allows all hostnames
       },
       {
-        protocol: 'https',
-        hostname: 'png.pngtree.com',
+        protocol: 'http',
+        hostname: '**', // Allows all hostnames
       },
     ],
   },
